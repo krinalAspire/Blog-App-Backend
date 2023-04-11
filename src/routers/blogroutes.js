72150f09c28024353c64ced3499bab0603762e13
krinalAspire <krinal.sonara@aspirespftserv.in>
2023-04-11 18:32:00 +0530

    routers

---
+++ src/routers/blogroutes.js
@@ -0,0 +1,64 @@
+const express=require("express");
+const router = new express.Router();
+const Blog=require("../models/blog");
+const auth=require("../middleware/auth");
+
+router.get("/blogs", async(req,res)=>{
+    try{
+        const blogData=await Blog.find();
+        res.status(201).send(blogData);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+router.get("/home",auth,(req,res)=>{
+    res.send("succesfull");
+})
+
+router.post("/blogs",async (req,res)=>{
+    try{
+        const blog=new Blog(req.body);
+        const createBlog= await blog.save();
+        res.status(201).send(createBlog);
+    }catch(e){
+        res.status(400).send(e)
+    }
+    
+})
+
+router.get("/blogs/:id",async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const blogData=await Blog.findById(_id );
+        res.status(201).send(blogData);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+router.patch("/blogs/:id",async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const updateBlog=await Blog.findByIdAndUpdate(_id, req.body,{
+            new:true
+        });
+        res.status(201).send(updateBlog);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+router.delete("/blogs/:id",async(req,res)=>{
+    try{
+        const deleteBlog=await Blog.findByIdAndDelete(req.params.id);
+        if(!req.params.id){
+            return res.status(400).send();
+        }
+        res.status(201).send(deleteBlog);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+module.exports=router;
---
+++ src/routers/userroutes.js
@@ -0,0 +1,188 @@
+const express=require("express");
+const router = new express.Router();
+const Register=require("../models/users");
+const bcrypt=require("bcryptjs");
+const jwt=require("jsonwebtoken");
+const nodemailer=require("nodemailer");
+
+//email config
+const transporter=nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:process.env.EMAIL,
+        pass:process.env.PASSWORD
+    }
+})
+
+router.post("/users",async (req,res)=>{
+    try{
+        const user=new Register(req.body);
+        const token=await user.generateAuthToken();
+        
+        // res.cookie("jwt",token,{
+        //     // expires:new Date(Date.now()+3000),
+        //     httpOnly:true
+        // });
+        const createUser= await user.save();
+        res.status(201).send(createUser);
+    }catch(e){
+        res.status(400).send(e)
+    }
+    
+})
+
+router.get("/users",async (req,res)=>{
+    try{
+        const userData=await Register.find();
+        res.status(201).send(userData);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+router.get("/users/:id",async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const userData=await Register.findById(_id);
+        res.status(201).send(userData);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+router.patch("/users/:id",async(req,res)=>{
+    try{
+        const _id=req.params.id;
+        const updateUser=await Register.findByIdAndUpdate(_id, req.body,{
+            new:true
+        });
+        res.status(201).send(updateUser);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+router.delete("/users/:id",async(req,res)=>{
+    try{
+        const deleteUser=await Register.findByIdAndDelete(req.params.id);
+        if(!req.params.id){
+            return res.status(400).send();
+        }
+        res.status(201).send(deleteUser);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+router.post('/login', async(req,res)=>{
+    const {userid, password} = req.body;
+try{
+    const userId = await Register.findOne({userid:userid})
+    const isMatch = await bcrypt.compare(password, userId.password)
+    const token = await userId.generateAuthToken()
+
+    if(isMatch){
+        res.status(201).json({token:token , data:userId});
+    }
+    else{
+        res.status(400).send(error)
+    }
+} 
+catch(e){
+    res.send(e)
+}
+})
+
+
+//send email link for reset password
+router.post("/sendpasswordlink", async(req,res)=>{
+    // console.log(req.body);
+    const {email}=req.body;
+
+    if(!email){
+        res.status(401).json({status:401, message:"Enter your Email"})
+    }
+
+    try{
+       const userFind=await Register.findOne({email:email});
+    //    console.log("userfind", userFind)
+    
+    //token generate for reset password
+    const token=jwt.sign({_id:userFind._id},process.env.SECRET_KEY,{
+        expiresIn:"120s"
+    })
+    //    console.log("token",token)   
+
+    const setuserToken=await Register.findByIdAndUpdate({_id:userFind._id},{verifytoken:token},{new:true})
+    // console.log("setusertoken",setuserToken);
+
+    if(setuserToken){
+        const mailOptions={
+            from:process.env.EMAIL,
+            to:email,
+            subject:"sending email for password reset",
+            text:`This Link Valid For 2 minutes http://localhost:3000/forgotpassword/${userFind.id}/${setuserToken.verifytoken}`,
+        }
+
+        transporter.sendMail(mailOptions,(error,info)=>{
+            if(error){
+                console.log("error",error);
+                res.status(401).json({status:401,message:"email not send"})
+            }else {
+                console.log("Email sent",info.response);
+                res.status(201).json({status:201,message:"Email sent Succesfully"} )
+            }
+        })
+    }
+    }catch(e){
+        res.status(401).json({status:401,message:"Invalid User"})
+    }
+})
+
+//varify user for forgot password time
+router.get("/forgotpassword/:id/:token", async(req,res)=>{
+    const {id,token}=req.params;
+    // console.log(id,token);
+
+    try{
+        const validuser=await Register.findOne({_id:id,verifytoken:token});
+        // console.log(validuser);
+
+        const verifyToken=jwt.verify(token,process.env.SECRET_KEY);
+        // console.log(verifyToken)
+        if(validuser && verifyToken._id){
+            res.status(201).json({status:201,validuser})
+        }else {
+            res.status(401).json({status:401, message:"user not exist"})
+        }
+    }catch(e){
+        res.status(401).json({status:401,e})
+    }
+})
+
+//change Password
+router.post("/:id/:token", async(req,res)=>{
+    const {id,token}=req.params;
+
+    const {password}=req.body;
+
+    try{
+        const validuser=await Register.findOne({_id:id,verifytoken:token});
+
+        const verifyToken=jwt.verify(token,process.env.SECRET_KEY);
+        
+        if(validuser && verifyToken._id){
+            const newPassword=await bcrypt.hash(password,10)
+            const setnewPass=await Register.findByIdAndUpdate({_id:id},{password:newPassword})
+
+            setnewPass.save();
+            res.status(201).json({status:201,setnewPass})
+        }else {
+            res.status(401).json({status:401, message:"user not exist"})
+        }
+    }catch(e){
+        res.status(401).json({status:401,e})
+    }
+})
+
+module.exports=router;
